27cf5ca51d91dc4bf993378d2d26af3fe3e25c65
ashkeen <ansariashkeen44@gmail.com>
2022-03-15 00:54:45 -0700

    first commit

---
+++ first task/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using first_task.Models;
+
+namespace first_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "*********** first task ************";
+            Class1 obj = new Class1();
+            obj.id = 1;
+            obj.name = "ashkeen";
+            obj.email = "ashkeen@gmail.com";
+            return View(obj);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ first task/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace first_task.Models
+{
+    public class Class1
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public string email { set; get; }
+    }
+}
